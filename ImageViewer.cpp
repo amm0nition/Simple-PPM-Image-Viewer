fb766eb13dd6470f74e869c4a215c5007822ba2a
Geo <63842114+amm0nition@users.noreply.github.com>
2025-12-06 00:16:34 +0700

    Added image scaling and window resize support

--- ImageViewer.cpp
+++ ImageViewer.cpp
@@ -2,6 +2,38 @@
 #include <SDL3\SDL.h> //Inspiration was done in SDL2, but I'm dumb so let's force it with SDL3 lmao
 #include <fstream>
 #include <string>
+#include <vector>
+
+void drawWindow(SDL_Surface* surface,
+	const SDL_PixelFormatDetails* fmt,
+	const std::vector<unsigned char>& pixels,
+	int width, int height, // original image size
+	int new_width, int new_height) // current window size
+{
+	SDL_Rect pixel{ 0, 0, 1, 1 };
+
+	
+	for (int y = 0; y < new_height; y++) {
+		for (int x = 0; x < new_width; x++) {
+
+			// nearest-neighbor image scaling
+			int src_x = (x * width) / new_width;
+			int src_y = (y * height) / new_height;
+
+			int idx = (src_y * width + src_x) * 3;
+
+			unsigned char r = pixels[idx];
+			unsigned char g = pixels[idx + 1];
+			unsigned char b = pixels[idx + 2];
+
+			Uint32 color = SDL_MapRGB(fmt, NULL, r, g, b);
+
+			pixel.x = x;
+			pixel.y = y;
+			SDL_FillSurfaceRect(surface, &pixel, color);
+		}
+	}
+}
 
 int main() {
 
@@ -29,35 +61,61 @@ int main() {
 
 	in.get();
 
+	std::vector<unsigned char> pixels(width * height * 3);
+	in.read((char*)pixels.data(), pixels.size());
+	in.close();  // done with the file
+
 	std::cout << "PPM Image Viewer!\n";
 	std::cout << "Image Width:" << width << "\n";
 	std::cout << "Image Height:" << height << "\n";
 
+	int original_width = width;
+	int original_height = height;
+
 	SDL_Window* window = SDL_CreateWindow("PPM Image Viewer", width, height, 0);
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 	const SDL_PixelFormatDetails* fmt = SDL_GetPixelFormatDetails(surface->format);
-
-	int x{};
-	int y{};
-
-	SDL_Rect pixel{x, y, 1, 1};
-	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-			unsigned char r = in.get();
-			unsigned char g = in.get();
-			unsigned char b = in.get();
-			Uint32 color = SDL_MapRGB(fmt, NULL, r, g, b);
-			pixel.x = x;
-			pixel.y = y;
-			SDL_FillSurfaceRect(surface, &pixel, color);
-		}
-	}
+	
+	drawWindow(surface, fmt, pixels, original_width, original_height, width, height);
 
 	while (app_running) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) { 
 			if (event.type == SDL_EVENT_QUIT) {
 				app_running = 0;
+			}
+			if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_I) {
+
+				width /= 1.2f;
+				height /= 1.2f;
+				if (width < 1) width = 1;
+				if (height < 1) height = 1;
+				
+				SDL_SetWindowSize(window, width, height); // resize the actual window
+				// update surface and format
+				surface = SDL_GetWindowSurface(window);
+				fmt = SDL_GetPixelFormatDetails(surface->format);
+				drawWindow(surface, fmt, pixels, original_width, original_height, width, height); 
+				system("cls"); // Windows only
+				std::cout << "Window size shrinked\n";
+				std::cout << "Image Width:" << width << "\n";
+				std::cout << "Image Height:" << height << "\n";
+			}
+			if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_O) {
+				width *= 1.2f;
+				height *= 1.2f;
+				if (width < 1) width = 1;
+				if (height < 1) height = 1;
+
+				SDL_SetWindowSize(window, width, height); // resize the actual window
+				// update surface and format
+				surface = SDL_GetWindowSurface(window);
+				fmt = SDL_GetPixelFormatDetails(surface->format);
+				drawWindow(surface, fmt, pixels, original_width, original_height, width, height);
+				system("cls"); // Windows only
+				std::cout << "Window size enlarged\n";
+				std::cout << "Image Width:" << width << "\n";
+				std::cout << "Image Height:" << height << "\n";
 			}
 		}
 		SDL_UpdateWindowSurface(window);
